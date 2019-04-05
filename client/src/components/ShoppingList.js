d27e2c68830225fd453e1837f41f043d9829642f
Victor Liu <victorliu716@gmail.com>
2019-04-05 15:35:22 -0700

    Implement redux to manage state of application

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button} from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import uuid from 'uuid';
 import { connect }  from 'react-redux';
-import { getItems } from '../actions/itemActions';
+import { getItems, deleteItem } from '../actions/itemActions';
 import PropTypes from 'prop-types';
 
 class ShoppingList extends Component {
@@ -11,37 +10,25 @@ class ShoppingList extends Component {
   componentDidMount() {
     this.props.getItems();
   }
-  
+
+  onDeleteClick = (id) => {
+    this.props.deleteItem(id);
+  }
+
   render() {
     const { items } = this.props.item;
     return(
       <Container>
-        <Button
-          color="dark"
-          style={{marginBottom: '2rem'}}
-          onClick={() => {
-            const name = prompt('Enter item');
-            if (name) {
-              this.setState(state => ({
-                items: [...state.items, { id: uuid(), name}]
-              }))
-            }
-          }}
-        >Add Item</Button>
         <ListGroup>
           <TransitionGroup className="shopping-list">
-            {items.map(({ id, name}) => (
-              <CSSTransition key={id} timeout={500} classNames="fade">
+            {items.map(({ _id, name}) => (
+              <CSSTransition key={_id} timeout={500} classNames="fade">
                 <ListGroupItem>
                   <Button
                     className="remove-btn"
                     color="danger"
                     size="sm"
-                    onClick={() => {
-                      this.setState(state => ({
-                        items: state.items.filter(item => item.id !== id)
-                      }));
-                    }}
+                    onClick={this.onDeleteClick.bind(this, _id)}
                   >&times;</Button>
                   {name}
                 </ListGroupItem>
@@ -63,4 +50,6 @@ const mapStateToProps = (state) => ({
   item: state.item
 })
 
-export default connect(mapStateToProps, {getItems})(ShoppingList);
+export default connect(
+  mapStateToProps, {getItems, deleteItem}
+  )(ShoppingList);
